10c0a2be039b504f93a0651c5ca48c0086ddca67
Chris Reed <chris.j.reed@hotmail.com>
2021-03-25 16:10:24 +0000

    Completed day 16

---
+++ Day_16/main.py
@@ -0,0 +1,24 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+options = menu.get_items()
+money_machine = MoneyMachine()
+coffe_maker = CoffeeMaker()
+is_on = True
+while is_on:
+    user_input = input(f"What would you like? ({options}): ").lower()
+    if user_input == "off":
+        is_on = False
+    elif user_input == "report":
+        coffe_maker.report()
+        money_machine.report()
+    else:
+        choice = menu.find_drink(user_input)
+        if choice != None:
+            is_enough = money_machine.make_payment(choice.cost)
+            if is_enough:
+                enough_resources = coffe_maker.is_resource_sufficient(choice)
+                if enough_resources:
+                    coffe_maker.make_coffee(choice)
