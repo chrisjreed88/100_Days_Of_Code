b335113d1fdc1d568608893665043c753bb301ce
Chris Reed <chris.j.reed@hotmail.com>
2021-06-15 15:48:05 +0100

    updated for day 30

--- Day_26/main.py
+++ Day_26/main.py
@@ -2,6 +2,17 @@ import pandas as pd
 
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for index, row in df.iterrows()}
-word = input("Enter a word: ").upper()
-nato_word = [nato_dict[char] for char in word]
-print(nato_word)
+
+
+def generate_nato():
+    word = input("Enter a word: ").upper()
+    try:
+        nato_word = [nato_dict[char] for char in word]
+    except KeyError:
+        print("Sorry, only letters please")
+        generate_nato()
+    else:
+        print(nato_word)
+
+
+generate_nato()
