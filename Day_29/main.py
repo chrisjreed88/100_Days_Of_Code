b99810b9b2dc297a8908b05d18a44b00c885a767
Chris Reed <chris.j.reed@hotmail.com>
2021-06-15 15:05:08 +0100

    sticky="ew" instaed of width

--- Day_29/main.py
+++ Day_29/main.py
@@ -76,26 +76,26 @@ email_label = Label(text="Email / Username:   ", bg="white", pady=3)
 password_label = Label(text="Password:", bg="white", pady=3)
 
 # Entries
-website_entry = Entry(width=52)
+website_entry = Entry()
 website_entry.focus()
-email_entry = Entry(width=52)
+email_entry = Entry()
 email_entry.insert(END, string="example@hotmail.com")
-password_entry = Entry(width=33)
+password_entry = Entry()
 
 # Buttons
 generate_button = Button(text="Generate Password", command=generate_password)
-add_button = Button(text="Add", width=44, command=save_data)
+add_button = Button(text="Add", command=save_data)
 
 # Add to grid
 canvas.grid(column=1, row=0)
 website_label.grid(column=0, row=1)
 email_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
-website_entry.grid(column=1, row=1, columnspan=2, sticky="w")
-email_entry.grid(column=1, row=2, columnspan=2, sticky="w")
-password_entry.grid(column=1, row=3, sticky="w")
-generate_button.grid(column=2, row=3)
-add_button.grid(column=1, row=4, columnspan=2, sticky="w")
+website_entry.grid(column=1, row=1, columnspan=2, sticky="ew")
+email_entry.grid(column=1, row=2, columnspan=2, sticky="ew")
+password_entry.grid(column=1, row=3, sticky="ew")
+generate_button.grid(column=2, row=3, sticky="ew")
+add_button.grid(column=1, row=4, columnspan=2, sticky="ew")
 
 
 window.mainloop()
